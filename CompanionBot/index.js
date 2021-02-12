61c04eb970c2587eda0fc7eedaaabe2011b873f6
magnusjw <magnusjw@clarkson.edu>
2021-02-12 10:51:19 -0500

    Adding Companion Bot

---
+++ CompanionBot/index.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const { PREFIX, TOKEN } = require('./config.json');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+    if (message.content === '!ping') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('Pong.');
+    }
+	console.log(message.content);
+});
+
+client.login(`${TOKEN}`);
