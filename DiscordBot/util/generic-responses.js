70f691343a8a9f46e975e71afa963660d3dfcdbe
magnusjw <magnusjw@clarkson.edu>
2021-02-12 15:52:42 -0500

    genericResponses

---
+++ DiscordBot/util/generic-responses.js
@@ -0,0 +1,31 @@
+
+
+let genericResponses = [
+    "Personally, it's ",
+    "Mine is ",
+    "I have to go with "
+    
+]
+
+
+let positiveResponses = [
+    "My favorite is ",
+    "My favorite one is ",
+    "The best one is "
+
+]
+
+
+let negativeResponses = [
+    "Ugh, its ",
+    "Unfortunately, its ",
+    "I guess it'd have to be ",
+
+]
+
+
+
+module.exports = {
+    genericResponses
+
+}
