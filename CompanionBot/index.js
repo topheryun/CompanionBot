d3e095dc48fccb595a78901fa4cd30db9e67ef95
magnusjw <magnusjw@clarkson.edu>
2021-02-12 11:25:19 -0500

    Base Command configured

--- CompanionBot/index.js
+++ CompanionBot/index.js
@@ -1,17 +1,40 @@
 const Discord = require('discord.js');
+const fs = require('fs');
 const { PREFIX, TOKEN } = require('./config.json');
 const client = new Discord.Client();
+
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
 client.on('message', message => {
-    if (message.content === '!ping') {
-        // send back "Pong." to the channel the message was sent in
-        message.channel.send('Pong.');
+
+    if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+    // console.log(message.author.username + ": " + message.content);
+
+    const args = message.content.slice(PREFIX.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    // the following 2 lines allow aliases
+    const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+    if (!command) return;
+
+    console.log(message.content);
+
+    try {
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command.');
     }
-	console.log(message.content);
 });
 
 client.login(`${TOKEN}`);
