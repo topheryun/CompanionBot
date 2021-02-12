4a4b08fd54f772ace7a27cdd6c3a91f17d0100ac
topheryun <topheryun@gmail.com>
2021-02-12 07:24:30 -0800

    first commit

---
+++ src/main/java/com/topheryun/CompanionBotServerApplication.java
@@ -0,0 +1,13 @@
+package com.topheryun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanionBotServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanionBotServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/topheryun/CompanionBotServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topheryun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanionBotServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
